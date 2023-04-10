fb26f3cc71c57aac55cba9f69fa1002dbb609acc
钟海秀(创新业务产品部) <zhonghaixiu12534@apexsoft.com.cn>
2023-04-10 20:04:40 +0800

    顶部tab菜单配置

--- src/layouts/workPlatForm/PageLayout/PageHeader/VisitedRoutes.js
+++ src/layouts/workPlatForm/PageLayout/PageHeader/VisitedRoutes.js
@@ -5,7 +5,6 @@ import { Icon, Dropdown, Menu } from 'antd';
 import { dropByCacheKey } from 'react-router-cache-route';
 import styles from './visitedRoutes.less';
 
-
 /**
  * 把树型结构数据转成扁平数据
  * @param source
@@ -16,7 +15,7 @@ function treeToArrayData(treeData, childrenName) {
     for (const i in source) {
       const { [childrenName]: children = [], ...primaryData } = source[i];
       arrayData.push(primaryData);
-      children && children.length > 0 ? treeDataToCompressed(children, childrenName) : '';// 子级递归
+      children && children.length > 0 ? treeDataToCompressed(children, childrenName) : ''; // 子级递归
     }
     return arrayData;
   };
@@ -37,7 +36,9 @@ function VisitedRoutes(props) {
   // 将树状结构转化为数组结构方便查找name
   const menuArray = treeToArrayData(menuTree, 'children');
 
-  const { location: { pathname = '', search = '' } } = history;
+  const {
+    location: { pathname = '', search = '' },
+  } = history;
   let newPathname = pathname + search;
   if (pathname && pathname.includes('/pms/manage/LifeCycleManagement')) {
     newPathname = '/pms/manage/LifeCycleManagement';
@@ -74,7 +75,7 @@ function VisitedRoutes(props) {
     setUrls([]);
   }, [projectName]);
 
-  const closePage = (url) => {
+  const closePage = url => {
     const newUrls = [...urls];
     const index = newUrls.indexOf(url);
     newUrls.splice(index, 1);
@@ -96,7 +97,7 @@ function VisitedRoutes(props) {
   };
 
   // 关闭其他页面
-  const closeOthers = (url) => {
+  const closeOthers = url => {
     setUrls([url]);
   };
 
@@ -108,18 +109,38 @@ function VisitedRoutes(props) {
   let tabCount = 0;
 
   return (
-    <div style={{width: '136px', height: '100%'}} className='clearfix' id='visited-scroll'>
+    <div style={{ width: '136px', height: '100%' }} className="clearfix" id="visited-scroll">
       {/* <div className={`cur-tabs ${styles.tagview} ${newPathname === homePage && styles.isActive}`}>
         <Link to={homePage}>
           <i className="iconfont icon-home" style={{ display: 'inline-block', height: '3.286rem' }} />
           <span style={{ margin: '0 1rem' }}>首页</span>
         </Link>
       </div> */}
-      {
-        urls.length > 0 && urls.map((item) => {
-          let {url = '', title = ''} = menuArray.find(m => m.url === item) || {};
+      {urls.length > 0 &&
+        urls.map(item => {
+          let { url = '', title = '' } = menuArray.find(m => m.url === item) || {};
+          if (item.includes('/pms/manage/ProjectDetail/')) {
+            url = item;
+            title = '项目详情';
+          }
+          if (item.includes('/pms/manage/ProjectInfo/')) {
+            url = item;
+            title = '项目列表';
+          }
+          if (item.includes('/pms/manage/staffDetail/')) {
+            url = item;
+            title = '人员详情';
+          }
+          if (item.includes('/pms/manage/labelDetail/')) {
+            url = item;
+            title = '标签详情';
+          }
+          if (item.includes('/pms/manage/attachLibrary/')) {
+            url = item;
+            title = '文档列表';
+          }
           if (title === '' && routerList.length > 0) {
-            const listIndex = routerList.findIndex((tempItem) => {
+            const listIndex = routerList.findIndex(tempItem => {
               return item.indexOf(tempItem.path) > -1;
             });
             title = listIndex >= 0 ? routerList[listIndex].note : '';
@@ -130,55 +151,63 @@ function VisitedRoutes(props) {
           }
           tabCount++;
           return (
-            <div key={url} className={`cur-tabs ${styles.tagview} `}
-              style={{ height: '100%' }}>
-              <div className = {`${styles.curTabsBak} ${newPathname === url && styles.isActive}`}>
+            <div key={url} className={`cur-tabs ${styles.tagview} `} style={{ height: '100%' }}>
+              <div className={`${styles.curTabsBak} ${newPathname === url && styles.isActive}`}>
                 <Link to={url}>{title}</Link>
-                <i className="iconfont icon-close" style={{marginLeft: '20px'}} onClick={() => {
-                  closePage(url);
-                }}/>
+                <i
+                  className="iconfont icon-close"
+                  style={{ marginLeft: '20px' }}
+                  onClick={() => {
+                    closePage(url);
+                  }}
+                />
               </div>
               {/* <Icon type="close-circle" theme="twoTone" twoToneColor="#ec6057"  /> */}
             </div>
           );
-        })
-      }
-      {
-        false && (
-          <Dropdown
-            overlay={
-              <Menu>
-                <Menu.Item>
-                  <a onClick={() => {
+        })}
+      {false && (
+        <Dropdown
+          overlay={
+            <Menu>
+              <Menu.Item>
+                <a
+                  onClick={() => {
                     closeAll();
-                  }}>
-                    关闭全部
-                  </a>
-                </Menu.Item>
-                <Menu.Item>
-                  <a onClick={() => {
+                  }}
+                >
+                  关闭全部
+                </a>
+              </Menu.Item>
+              <Menu.Item>
+                <a
+                  onClick={() => {
                     closeOthers(newPathname);
-                  }}>
-                    关闭其他
-                  </a>
-                </Menu.Item>
-              </Menu>
-            }
-          >
-            <div ref={moreIconRef} className={styles.tagview} style={{
+                  }}
+                >
+                  关闭其他
+                </a>
+              </Menu.Item>
+            </Menu>
+          }
+        >
+          <div
+            ref={moreIconRef}
+            className={styles.tagview}
+            style={{
               position: 'absolute',
               left: visitedScroll,
               zIndex: 2,
               background: '#ffffff',
               borderLeft: 'none',
               height: '100%',
-              padding: '0 4px'
-            }}>
-              <Icon type="more" style={{ fontSize:12, margin: 0, color: '#1e2536' }} />
-            </div>
-          </Dropdown>
-        )
-      }
+              padding: '0 4px',
+            }}
+          >
+            <Icon type="more" style={{ fontSize: 12, margin: 0, color: '#1e2536' }} />
+          </div>
+        </Dropdown>
+      )}
     </div>
   );
 }
