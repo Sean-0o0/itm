0a29c69f348f79c03b84aac48613bf78a5e708f3
钟海秀(创新业务产品部) <zhonghaixiu12534@apexsoft.com.cn>
2023-04-18 18:40:02 +0800

    硬件合同上传入口配置

--- src/components/pmsPage/ProjectDetail/MileStone/ItemBtn/index.js
+++ src/components/pmsPage/ProjectDetail/MileStone/ItemBtn/index.js
@@ -652,6 +652,7 @@ class ItemBtn extends React.Component {
       case '生产安装部署手册':
       case '生产操作及运维手册':
       case '用户手册':
+      case '硬件合同上传':
         return this.getWdscxg(done, item);
 
       //其他
