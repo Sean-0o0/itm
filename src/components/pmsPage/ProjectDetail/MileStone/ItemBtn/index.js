cf293d9289bd8017a2d6f4e6ec1ad1f324588e5d
chenjian <chenjian1@apexsoft.com.cn>
2023-04-19 17:36:56 +0800

    硬件项目弹窗样式修改

--- src/components/pmsPage/ProjectDetail/MileStone/ItemBtn/index.js
+++ src/components/pmsPage/ProjectDetail/MileStone/ItemBtn/index.js
@@ -729,7 +729,7 @@ class ItemBtn extends React.Component {
       title: lbModalTitle,
       width: '1000px',
       height: '600px',
-      style: { top: '60px' },
+      style: {top: '20px'},
       visible: true,
       footer: null,
     };
@@ -739,8 +739,8 @@ class ItemBtn extends React.Component {
       isAllWindow: 1,
       title: lbModalTitle,
       width: '1000px',
-      height: '600px',
-      style: { top: '60px' },
+      height: '770px',
+      style: {top: '20px'},
       visible: true,
       footer: null,
     };
