3f24eb1ffb308846d6024cf29160daf61bc5470d
钟海秀(创新业务产品部) <zhonghaixiu12534@apexsoft.com.cn>
2023-11-03 16:52:12 +0800

    1、信创合同信息列表； 2、信创合同信息编辑页面；
    (cherry picked from commit a94f05e2ca9fff60252bf2a6747fa38d0feb427b)

---
+++ src/pages/pmsPage/InnovationContract/index.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import InnovationContractTab from '../../../components/pmsPage/InnovationContract/index';
+export default props => <InnovationContractTab {...props} />;
---
+++ src/pages/pmsPage/InnovationContractEdit/index.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import InnovationContractEditTab from '../../../components/pmsPage/InnovationContractEdit/index';
+export default props => <InnovationContractEditTab {...props} />;
